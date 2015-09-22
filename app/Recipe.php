ac3b7b1b6f3101ec82a09e04f6873345a107f3e7
Hugo Pereia <hugopereira84@gmail.com>
2015-09-23 00:30:18 +0100

    fix last line

--- app/Recipe.php
+++ app/Recipe.php
@@ -110,30 +110,28 @@ class Recipe extends Model
         
             
         $offsetHeader = $request->header('offset');
-        $offset = isset($offsetHeader) && !empty($offsetHeader) ? $offsetHeader : 1;
+        $offset = isset($offsetHeader) && !empty($offsetHeader) ? $offsetHeader : 0;
         
         $limitHeader = $request->header('limit');
         $limit = isset($limitHeader) && !empty($limitHeader) ? $limitHeader : 5;
         
         
-        $csv            = self::loadXlsFile();
-        $csv->setOffset(1);
+        $csv = self::loadXlsFile();
+        //$csv->setOffset(1);
         
         $arrNameFieldsCsv = self::modelFieldsExists($columns);
         
         
-        //make pagination
-        $csv->setOffset($offset);
-        $csv->setLimit($limit);
+
         
         //Validate that row of csv has all values
         $csv->addFilter(function ($row) {
-                        return isset($row[0], $row[1], $row[2], $row[3], $row[4], 
-                                    $row[5], $row[6], $row[7], $row[8], $row[9], 
-                                    $row[10], $row[11], $row[12], $row[13], $row[14], 
-                                    $row[15], $row[16], $row[17], $row[18], $row[19], 
+                        return isset($row[0], $row[1], $row[2], $row[3], $row[4],
+                                    $row[5], $row[6], $row[7], $row[8], $row[9],
+                                    $row[10], $row[11], $row[12], $row[13], $row[14],
+                                    $row[15], $row[16], $row[17], $row[18], $row[19],
                                     $row[20], $row[21], $row[22], $row[23], $row[24],
-                                    $row[25]); 
+                                    $row[25]);
                     });
                     
         //make a where in the csv file
@@ -142,12 +140,17 @@ class Recipe extends Model
             $keyValueSelected = array_search($nameField, $lstFieldsModel);
             $extraParams['keyValueSelected'] = $keyValueSelected;
             $csv->addFilter(function ($row) use ($extraParams) {
-                echo $extraParams['valueField'] ."/". $row[$extraParams['keyValueSelected']].PHP_EOL;
-                return $extraParams['valueField'] == $row[$extraParams['keyValueSelected']]; 
+                return (string)$extraParams['valueField'] == (string)$row[$extraParams['keyValueSelected']];
             });
         }
+
+        //make pagination
+        $csv->setOffset($offset);
+        $csv->setLimit($limit);
+
+        //get results
         $result = $csv->fetchAssoc($arrNameFieldsCsv);
-        
+
                   
         return $result;
     }
